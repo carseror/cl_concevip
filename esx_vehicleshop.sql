7e614aeda111cb93616a1c0d9dcbaf7748d80ddc
carseror <77501586+carseror@users.noreply.github.com>
2021-10-27 21:29:33 +0200

    Add files via upload

---
+++ esx_vehicleshop.sql
@@ -0,0 +1,32 @@
+USE `sesx`;
+
+CREATE TABLE `vehiclevip_categories` (
+	`name` varchar(60) NOT NULL,
+	`label` varchar(60) NOT NULL,
+
+	PRIMARY KEY (`name`)
+);
+
+INSERT INTO `vehiclevip_categories` (name, label) VALUES
+	('compacts','Compacts'),
+	('coupes','Coupés'),
+	('sedans','Sedans'),
+	('sports','Sports'),
+	('sportsclassics','Sports Classics'),
+	('super','Super'),
+	('muscle','Muscle'),
+	('offroad','Off Road'),
+	('suvs','SUVs'),
+	('vans','Vans'),
+	('motorcycles','Motos')
+;
+
+CREATE TABLE `vehiclesvip` (
+	`name` varchar(60) NOT NULL,
+	`model` varchar(60) NOT NULL,
+	`price` int(11) NOT NULL,
+	`category` varchar(60) DEFAULT NULL,
+
+	PRIMARY KEY (`model`)
+);
+
